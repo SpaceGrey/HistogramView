393efdbe6d785e1cb5dc1e814d25ebc301ad5b63
SpaceGrey <78807862+SpaceGrey@users.noreply.github.com>
2023-12-31 23:28:49 +0800

    recover works

--- Sources/HistogramView/HistogramChannel.swift
+++ Sources/HistogramView/HistogramChannel.swift
@@ -11,26 +11,48 @@ struct HistogramChannel: Shape {
 
     let data: [UInt]
     let scale: CGFloat
-
+    let orientation:UIDeviceOrientation
+    
     private var maximum: UInt { data.max() ?? 0 }
-
+    
     func path(in rect: CGRect) -> Path {
+        
         Path { path in
-            path.move(to: CGPoint(x: 0, y: rect.height))
-
             let interpolationPoints: [CGPoint] = data.enumerated().map { (index, element) in
-                let y = rect.height - (CGFloat(element) / CGFloat(maximum) * rect.height) * scale
-                let x = CGFloat(index) / CGFloat(data.count) * rect.width
+                let y = 1 - (CGFloat(element) / CGFloat(maximum)) * scale
+                let x = CGFloat(index) / CGFloat(data.count)
                 return CGPoint(x: x, y: y)
             }
-
-            guard let curves = Path.interpolatePointsWithHermite(interpolationPoints: interpolationPoints) else {
-                return
+            switch orientation {
+            case .portrait,.unknown,.faceUp,.faceDown:
+                path.move(to: CGPoint(x: 0, y: rect.height))
+                path.addLines(interpolationPoints.map{
+                    CGPoint(x: $0.x * rect.width, y: $0.y * rect.height)
+                })
+                path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+                path.addLine(to: CGPoint(x: 0, y: rect.height))
+            case .portraitUpsideDown:
+                path.move(to: CGPoint(x: rect.width, y: 0))
+                path.addLines(interpolationPoints.map{
+                    CGPoint(x: rect.width - $0.x * rect.width, y: rect.height - $0.y * rect.height)
+                })
+                path.addLine(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: rect.width, y: 0))
+            case .landscapeRight:
+                path.move(to: CGPoint(x: rect.width, y: rect.height))
+                path.addLines(interpolationPoints.map{
+                   CGPoint(x: $0.y * rect.width, y: (1-$0.x) * rect.height)
+                })
+                path.addLine(to: CGPoint(x: rect.width, y: 0))
+                path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            case .landscapeLeft:
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLines(interpolationPoints.map{
+                    CGPoint(x: rect.width - $0.y * rect.width, y: rect.height - (1-$0.x) * rect.height)
+                })
+                path.addLine(to: CGPoint(x: 0, y: rect.height))
+                path.addLine(to: CGPoint(x: 0, y: 0))
             }
-
-            path.addPath(curves)
-            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
-            path.addLine(to: CGPoint(x: 0, y: rect.height))
         }
     }
 
